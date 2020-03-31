5d185005c984d6fdea31accbd9b23002b78dbd72
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-30 18:30:36 -0700

    update readme

--- src/services/__mocks__/githubApi.js
+++ src/services/__mocks__/githubApi.js
@@ -2,6 +2,7 @@ export const getFetchUser = () => {
   return Promise.resolve({ login: 'licarpen', following: 8 });
 };
 
+// eslint-disable-next-line no-unused-vars
 export const getFetchUserRepos = (user) => {
   return Promise.resolve([{ name: 'lab1' }, { name: 'lab2' }]);
 };
